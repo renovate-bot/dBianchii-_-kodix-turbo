d5387d17bb80818ce6c1ef0f2a0299d2f993e320
dbianchii <gdbianchii@gmail.com>
2023-07-18 12:34:24 -0300

    Copy nextJs to kodix-care

---
+++ apps/kodix-care/next.config.mjs
@@ -0,0 +1,15 @@
+// Importing env files here to validate on build
+import "./src/env.mjs";
+import "@kdx/auth/env.mjs";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@kdx/api", "@kdx/auth", "@kdx/db"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
