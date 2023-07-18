6f0253c4fbcc20e73cb849cd26387425675f12ef
dbianchii <gdbianchii@gmail.com>
2023-07-17 22:34:13 -0300

    Auth package

--- packages/auth/env.mjs
+++ packages/auth/env.mjs
@@ -3,8 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    DISCORD_CLIENT_ID: z.string().min(1),
-    DISCORD_CLIENT_SECRET: z.string().min(1),
+    DATABASE_URL: z.string().url(),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
@@ -16,13 +15,28 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
+		GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string(),
+    EMAIL_SERVER_USER: z.string(),
+    EMAIL_SERVER_PASSWORD: z.string(),
+    EMAIL_SERVER_HOST: z.string(),
+    EMAIL_SERVER_PORT: z.string(),
+    EMAIL_FROM: z.string(),
+    OPENAI_API_KEY: z.string(),
   },
   client: {},
   runtimeEnv: {
+		DATABASE_URL: process.env.DATABASE_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_SERVER_HOST: process.env.EMAIL_SERVER_HOST,
+    EMAIL_SERVER_PORT: process.env.EMAIL_SERVER_PORT,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
   skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
 });
--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,7 +1,8 @@
-import Discord from "@auth/core/providers/discord";
 import type { DefaultSession } from "@auth/core/types";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import GoogleProvider from "next-auth/providers/google";
 
 import { prisma } from "@kdx/db";
 
@@ -10,15 +11,20 @@ import { env } from "./env.mjs";
 export type { Session } from "next-auth";
 
 // Update this whenever adding new providers so that the client can
-export const providers = ["discord"] as const;
+export const providers = ["google", "email"] as const;
 export type OAuthProviders = (typeof providers)[number];
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+			// ...other properties
+      // role: UserRole;
+      activeWorkspaceId: string; // Might need fix
+      activeWorkspaceName: string;
     } & DefaultSession["user"];
   }
+
 }
 
 export const {
@@ -28,19 +34,43 @@ export const {
 } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
-    Discord({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+    EmailProvider({
+			server: {
+				host: env.EMAIL_SERVER_HOST,
+				port: env.EMAIL_SERVER_PORT,
+				auth: {
+					user: env.EMAIL_SERVER_USER,
+					pass: env.EMAIL_SERVER_PASSWORD,
+				},
+			},
+			from: env.EMAIL_FROM,
+			type: "email",
+			sendVerificationRequest: () => {
+				throw new Error("Not implemented");
+			},
+			id: "",
+			name: ""
+		}),
   ],
   callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
+    async session({ session, user }) {
+      if (session.user) {
+        const workspace = await prisma.workspace.findUnique({
+          where: {
+            id: session.user.activeWorkspaceId,
+          },
+        });
+        session.user.id = user.id;
+        //session.user.activeWorkspaceId = user.activeWorkspaceId; // Might need fix
+
+        session.user.activeWorkspaceName = workspace?.name ?? "";
+      }
+      return session;
+    },
 
     // @TODO - if you wanna have auth on the edge
     // jwt: ({ token, profile }) => {
@@ -55,5 +85,36 @@ export const {
     // authorized({ request, auth }) {
     //   return !!auth?.user
     // }
+  },
+	events: {
+    createUser: async (message) => {
+      const firstName = message.user.name ? message.user.name.split("")[0] : "";
+      //Create a personal workspace for the user on signup, set it as their active workspace
+      const workspace = await prisma.workspace.create({
+        data: {
+          name: `${firstName ?? ""}'s Workspace`,
+          users: {
+            connect: [{ id: message.user.id }],
+          },
+        },
+      });
+
+      await prisma.user.update({
+        where: {
+          id: message.user.id,
+        },
+        data: {
+          activeWorkspaceId: workspace.id,
+        },
+      });
+    },
+  },
+	secret: env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signIn",
+    //signOut: '/auth/signout',
+    //error: '/auth/error', // Error code passed in query string as ?error=
+    //verifyRequest: '/auth/verify-request', // (used for check email message)
+    //newUser: "/auth/new-user"
   },
 });
