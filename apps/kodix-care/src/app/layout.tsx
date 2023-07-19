6799e5c8c4b6d06db154abbc12a66c528e436cb6
gdbianchii <gdbianchii@gmail.com>
2023-07-19 13:30:56 -0300

    Try to remove auth

--- apps/kodix-care/src/app/layout.tsx
+++ apps/kodix-care/src/app/layout.tsx
@@ -4,21 +4,21 @@ import "~/styles/globals.css";
 
 import { TRPCReactProvider } from "./providers";
 
-// export const metadata: Metadata = {
-//   title: "Create T3 Turbo",
-//   description: "Simple monorepo with shared backend for web & mobile apps",
-//   openGraph: {
-//     title: "Create T3 Turbo",
-//     description: "Simple monorepo with shared backend for web & mobile apps",
-//     url: "https://create-t3-turbo.vercel.app",
-//     siteName: "Create T3 Turbo",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     site: "@jullerino",
-//     creator: "@jullerino",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Create T3 Turbo",
+  description: "Simple monorepo with shared backend for web & mobile apps",
+  openGraph: {
+    title: "Create T3 Turbo",
+    description: "Simple monorepo with shared backend for web & mobile apps",
+    url: "https://create-t3-turbo.vercel.app",
+    siteName: "Create T3 Turbo",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@jullerino",
+    creator: "@jullerino",
+  },
+};
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
--- apps/kodix-care/src/app/page.tsx
+++ apps/kodix-care/src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from "react";
 
-import { auth } from "@kdx/auth";
+// import { auth } from "@kdx/auth";
 
-import { SignIn, SignOut } from "~/components/auth";
+//import { SignIn, SignOut } from "~/components/auth";
 import { CreatePostForm, PostList } from "./posts";
 
 export default function HomePage() {
@@ -12,7 +12,7 @@ export default function HomePage() {
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Create <span className="text-pink-400">T3</span> Turbo
         </h1>
-        <AuthShowcase />
+        {/* <AuthShowcase /> */}
 
         <CreatePostForm />
         <Suspense fallback={<span>Loading...</span>}>
@@ -23,29 +23,29 @@ export default function HomePage() {
   );
 }
 
-async function AuthShowcase() {
-  const session = await auth();
+// async function AuthShowcase() {
+//   const session = await auth();
 
-  if (!session) {
-    return (
-      <SignIn
-        provider="google"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-      >
-        Sign in with Google
-      </SignIn>
-    );
-  }
+//   if (!session) {
+//     return (
+//       <SignIn
+//         provider="google"
+//         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+//       >
+//         Sign in with Google
+//       </SignIn>
+//     );
+//   }
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {session && <span>Logged in as {session.user.name}</span>}
-      </p>
+//   return (
+//     <div className="flex flex-col items-center justify-center gap-4">
+//       <p className="text-center text-2xl text-white">
+//         {session && <span>Logged in as {session.user.name}</span>}
+//       </p>
 
-      <SignOut className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
-        Sign out
-      </SignOut>
-    </div>
-  );
-}
+//       <SignOut className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20">
+//         Sign out
+//       </SignOut>
+//     </div>
+//   );
+// }
